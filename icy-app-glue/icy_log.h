cd71a2e1da0f78c7e68c3ca2529ecedbff72644f
Jake Stine <jake.stine@gmail.com>
2021-01-02 07:56:38 -0800

    icy_log: simple printf logging added

---
+++ icy-app-glue/icy_log.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// the fflush() bookends are needed on log_error on windows platforms, otherwise stdout and stderr outputs will
+// issue out-of-order to the users' console TTY. (unix'y platforms may handle this gracefully behind the scenes?)
+
+#if !defined(log_host)
+#   define log_host(fmt, ...)        (                 fprintf(stdout, fmt "\n", __VA_ARGS__), fflush(nullptr))
+#endif
+#if !defined(log_error)
+#   define log_error(fmt, ...)       (fflush(nullptr), fprintf(stderr, fmt "\n", __VA_ARGS__), fflush(nullptr))
+#endif
+
